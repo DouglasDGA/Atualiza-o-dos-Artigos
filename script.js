fee5c8516ee20d9a760f58f530c88625c76cc50a
DouglasDG <114673280+DouglasDGA@users.noreply.github.com>
2022-10-11 14:33:30 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,92 @@
+var $h1 = document.getElementById("Subtitulo");
+
+var $div1 = document.getElementById("Subtitulo");
+
+var $h1 = document.getElementById("Subtitulo").style.color = "#ff1919";
+for(var i = 0; i < 1000; i++);
+for(var i = 0; i < 255; i++)document.getElementById("Subtitulo").style.color = "#ff1919" + i;
+
+console.log($div1.firstChild);
+console.log($h1.textContent);
+console.log($h1);
+
+div1 = document.getElementById("Subtitulo");
+
+//alert(div)
+$div1.innerHTML = "Feito por Douglas D.G. de Albuquerque";
+
+
+
+var $h1 = document.getElementById("Subtitulo-2");
+
+var $div2 = document.getElementById("Subtitulo-2");
+
+var $h1 = document.getElementById("Subtitulo-2").style.color = "#ff1919";
+for(var i = 0; i < 1000; i++);
+for(var i = 0; i < 255; i++)document.getElementById("Subtitulo-2").style.color = "#ff1919" + i;
+
+console.log($div2.firstChild);
+console.log($h1.textContent);
+console.log($h1);
+
+div2 = document.getElementById("Subtitulo-2");
+
+//alert(div)
+$div2.innerHTML = "A Sua Continuação";    
+
+
+
+var $h1 = document.getElementById("Conclusao");
+
+var $div3 = document.getElementById("Conclusao");
+
+var $h1 = document.getElementById("Conclusao").style.color = "#ff1919";
+for(var i = 0; i < 1000; i++);
+for(var i = 0; i < 255; i++)document.getElementById("Conclusao").style.color = "#ff1919" + i;
+
+console.log($div3.firstChild);
+console.log($h1.textContent);
+console.log($h1);
+
+div3 = document.getElementById("Conclusao");
+
+//alert(div)
+$div3.innerHTML = "Conclusão";
+
+
+
+var $h1 = document.getElementById("Informativo");
+
+var $div4 = document.getElementById("Informativo");
+
+var $h1 = document.getElementById("Informativo").style.color = "#ff1919";
+for(var i = 0; i < 1000; i++);
+for(var i = 0; i < 255; i++)document.getElementById("Informativo").style.color = "#ff1919" + i;
+
+console.log($div4.firstChild);
+console.log($h1.textContent);
+console.log($h1);
+
+div4 = document.getElementById("Informativo");
+
+//alert(div)
+$div4.innerHTML = "Informativo";
+
+
+
+var $h2 = document.getElementById("Mensagem");
+
+var $dir = document.getElementById("Mensagem");
+
+var $h2 = document.getElementById("Mensagem").style.color = "#ae00ff";
+for(var i = 0; i < 1000; i++);
+for(var i = 0; i < 255; i++)document.getElementById("Mensagem").style.color = "#ae00ff" + i;
+
+console.log($dir.firstChild);
+console.log($h2.textContent);
+console.log($h2);
+
+dir = document.getElementById("Mensagem");
+
+//alert(dir)
+$dir.innerHTML = "Agradecimentos";
